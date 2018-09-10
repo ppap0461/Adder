821073d2ddacd6c1de70e0aff745d633321a28e3
Unknown <unknown@example.com>
2018-09-10 18:48:13 +0700

    add.adder.java

---
+++ Adder.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Adder {
+ public static void main(String[] args) {
+   Scanner sc = new Scanner(System.in);
+   int x,y,z;
+    System.out.println("enter x=>");
+    x = sc.nextInt();
+    System.out.println("enter y=>");
+    y = sc.nextInt();
+    z = x+y ;
+    System.out.println(z);
+
+  }
+}
